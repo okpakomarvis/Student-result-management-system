fea292f5d280937eca1e608be00e94e2c0561b6a
okpakomarvis <okpakomarvis@gmail.com>
2022-09-21 14:39:22 +0100

    first commit

---
+++ dashboard/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from dashboard import views
+
+app_name = 'dashboard'
+
+urlpatterns = [
+    path('', views.DashboardView.as_view(), name='dashboard'),
+    path('find-result/', views.find_result_view, name='find_result'),
+    path('change-password/', views.PasswordChangeView.as_view(), name='change_password'),
+    path('find-result/<int:pk>/result/', views.result, name='get_result'),
+    path('pdf/<int:id>',views.pdf.as_view() , name="pdf")
+]
